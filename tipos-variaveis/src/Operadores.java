46598d85621b4c631f37c740e8a1095061c9ef72
Rafael Vicente <rafael.s_vicente@hotmail.com>
2024-06-27 22:21:40 -0300

    Adicionado novo projeto sobre metodos

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,11 @@
+public class Operadores {
+    public static void main(String[] args) {
+        int numero = 5;
+            // repetição
+
+        numero++;
+        //mesma coisa que executar "numero + numero +1;"
+
+        System.out.println(numero);
+    }
+}
